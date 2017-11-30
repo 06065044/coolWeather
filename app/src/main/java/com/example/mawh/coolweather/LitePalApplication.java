5923b3a63f2d65514ccb8a8d4cc07c23ed6853f8
whma <mawh@tkoffice.local>
2017-11-30 19:53:22 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/mawh/coolweather/LitePalApplication.java
@@ -0,0 +1,14 @@
+package com.example.mawh.coolweather;
+
+import android.app.Application;
+
+/**
+ * Created by mawh on 2017/11/30.
+ */
+
+public class LitePalApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
